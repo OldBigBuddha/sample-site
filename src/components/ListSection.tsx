456327bf2a4aad60c5b65e2d6c1fc78e151fe8db
OldBigBuddha <development@oldbigbuddha.net>
2020-12-17 13:16:19 +0900

    refactor: 💡 セクションタイトルにアンダーラインを追加した

--- src/components/ListSection.tsx
+++ src/components/ListSection.tsx
@@ -21,7 +21,7 @@ const samples = [
 
 const ListSection: React.FC<Props> = ({ title }) => (
   <section className="px-24 py-12 bg-red-200">
-    <h2 className="text-4xl font-bold text-center mb-8">{title}</h2>
+    <h2 className="text-4xl font-bold text-center underline mb-8">{title}</h2>
     <ul className="flex justify-around">
       {samples.map(sample => <ListSectionItem title={sample.title} description={sample.description} />)}
     </ul>
